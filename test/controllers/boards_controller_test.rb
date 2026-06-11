fa324e38518ac44f6ff4ccae596cd883fad1bf79
knb <kensei@artifarm.com>
2026-06-12 04:19:29 +0900

    Refine board card and schedule list chrome

--- test/controllers/boards_controller_test.rb
+++ test/controllers/boards_controller_test.rb
@@ -12,13 +12,18 @@ class BoardsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "show renders kanban" do
-    get board_url(boards(:one))
+    board = boards(:one)
+    BoardKanban::AddMemo.call(board: board, memo: memos(:one), column: board_columns(:one_todo))
+
+    get board_url(board)
     assert_response :success
     assert_includes response.body, board_columns(:one_todo).name
     assert_select "dialog#board_add_memo_panel[aria-labelledby='board-add-memo-title']"
     assert_select "#board-add-memo-title", text: "既存メモを追加"
     assert_select "search label.sr-only[for='q']", text: "追加する既存メモを検索"
     assert_select "search input[type='search'][data-board-add-memo-target='query']"
+    assert_select ".kb-kanban-card button[aria-label='ドラッグ'].border-0.bg-transparent.p-0"
+    assert_select ".kb-kanban-card button[title='ボードから外す'].border-0.bg-transparent.p-0"
   end
 
   test "show renders schedule sidebar with calendar" do
@@ -54,6 +59,7 @@ class BoardsControllerTest < ActionDispatch::IntegrationTest
     get board_url(board, schedule_month: "2026-05", schedule_day: "2026-05-15")
     assert_response :success
     assert_includes response.body, memo.title
+    assert_select ".kb-board-schedule-list ul.list-none.p-0"
   end
 
   test "create board with default columns" do
